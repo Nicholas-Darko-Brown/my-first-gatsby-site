76a5d2b3e59b16cf2dbb9a3c65da2fe06507f417
Nicholas Darko Brown <87429177+Nicholas-Darko-Brown@users.noreply.github.com>
2022-11-01 15:50:26 +0000

    refactor: edit layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,19 +3,19 @@ import { Link, useStaticQuery, graphql } from 'gatsby'
 import { LayoutProps } from '../types/models'
 
 const Layout = ({ pageTitle, children }: any) => {
-    const data: LayoutProps = useStaticQuery(graphql`
-        query MyQuery {
-            site {
-                siteMetadata {
-                    title
-            }
-        }
-    }
-    `)
+    // const data: LayoutProps = useStaticQuery(graphql`
+    //     query MyQuery {
+    //         site {
+    //             siteMetadata {
+    //                 title
+    //         }
+    //     }
+    // }
+    // `)
 
     return (
         <div className='flex flex-col justify-center items-center py-2'>
-            <header className='my-2 border px-2 text-2xl text-underline'> {data.site?.siteMetadata.title} </header>
+            <header className='my-2 border px-2 text-2xl text-underline'> Gatsby Site </header>
             <nav>
                 <ul className='flex gap-2'>
                     <li className='bg-blue-300 px-3 rounded hover:bg-blue-400'><Link to="/">Home</Link></li>
